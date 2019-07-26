44d775b137f012429c08b14c8d1bc71b7909a0a0
Tomasz Sienkiewicz <tomasz.sienkiewicz@mail.com>
2019-07-26 11:39:23 +0200

    feat: add css and js

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -17,6 +17,31 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
-console.log(`Hello world!`);
+const buttonAdd = document.querySelector('.button-add--js');
+const buttonRemove = document.querySelector('.button-remove--js');
+const value = document.querySelector('.counter__value--js');
+const key = new Date().toISOString().slice(0, 10);
+
+if (!localStorage.getItem(key)) {
+  localStorage.setItem(key, 0)
+  value.innerHTML = '0';
+} else {
+  value.innerHTML = localStorage.getItem(key);
+}
+
+buttonAdd.addEventListener('click', (e) => {
+  localStorage.setItem(key, parseInt(localStorage.getItem(key)) + 1);
+  value.innerHTML = localStorage.getItem(key);
+})
+
+buttonRemove.addEventListener('click', (e) => {
+  const currentValue = parseInt(localStorage.getItem(key))
+  if (currentValue > 0) {
+    localStorage.setItem(key, localStorage.getItem(key) - 1)
+    value.innerHTML = localStorage.getItem(key);
+  }
+})
+
+
 
 
